aeafe5905fa42d4ad6f6613fb2823738b6b8a5fc
SeyedAliPoorhosseini <seyedalipoorhoseiny@gmail.com>
2025-01-03 18:47:08 +0330

    [UPDATE]  reverse-array.ts

--- hello-typescript/src/reverse-array.ts
+++ hello-typescript/src/reverse-array.ts
@@ -1,4 +1,4 @@
-function reverseArray(numbers: number[]): number[] {
+function reverseArray<T>(numbers: T[]): T[] {
   const numbersReverse = [];
   for (let i = numbers.length - 1; i >= 0; i--) {
     numbersReverse.push(numbers[i]);
@@ -6,3 +6,5 @@ function reverseArray(numbers: number[]): number[] {
   return numbersReverse;
 }
 console.log(reverseArray([1, 2, 3]));
+console.log(reverseArray(["a", "b", "c"]));
+console.log(reverseArray([true, false]));
