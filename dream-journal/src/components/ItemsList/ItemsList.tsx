2cfc39d016f15cb3f0c8146f186a53c45494944f
SeyedAliPoorhosseini <seyedalipoorhoseiny@gmail.com>
2025-01-16 14:29:43 +0330

    [ADD]  items list styles

--- dream-journal/src/components/ItemsList/ItemsList.tsx
+++ dream-journal/src/components/ItemsList/ItemsList.tsx
@@ -1,4 +1,30 @@
+import IconParkTwotoneDelete from "../../icons/IconParkTwotoneDelete";
+import IconParkTwotoneWrite from "../../icons/IconParkTwotoneWrite";
+
+import styles from "./ItemsList.module.css";
+
+const dreams = ["Note #1", "Note #2", "Note #3"];
+
 function ItemsList() {
-  return <ul className="items"></ul>;
+  return (
+    <ul className={styles.items}>
+      {dreams.map((dream) => (
+        <li key={dream}>
+          <label>
+            <input type="checkbox" />
+            <p>{dream}</p>
+          </label>
+          <div className={styles.actions}>
+            <button className={styles.edit}>
+              <IconParkTwotoneWrite />
+            </button>
+            <button className={styles.remove}>
+              <IconParkTwotoneDelete />
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
 export default ItemsList;
